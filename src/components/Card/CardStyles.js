d726fc4261f1067e06e2ad11f5dd84e10efb583a
DavidPalacio99 <dpalacioh@unal.edu.co>
2022-12-28 20:40:00 -0500

    First commit, setting basic configs of the project

---
+++ src/components/Card/CardStyles.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardStyles = () => {
+  return <div>CardStyles</div>;
+};
+
+export default CardStyles;
---
+++ src/components/globalStyles.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+body{
+  margin:0;
+  background: ${(props) => {
+    return props.theme.background;
+  }};
+  color:${({ theme }) => theme.colorText};
+  transition:all 0.50s linear;
+  font:${({ theme }) => theme.typographyTitle};
+}
+
+button{
+  border: none;
+}
+
+h1{
+  margin: 0;
+}
+
+`;
+
+// export const GlobalStyles = createGlobalStyle`
+
+// body{
+//   margin:0;
+//   background: red;
+//   color: blue;
+//   transition:all 0.50s linear;
+// }
+
+// button{
+//   border: none;
+// }
+
+// h1{
+//   margin: 0;
+// }
+
+// `;
